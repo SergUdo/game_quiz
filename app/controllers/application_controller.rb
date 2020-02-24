0da457eb9232ea6f0b2f9574ee48cd4ff2c7fefb
SergUdo <sergii.udovichenko@gmail.com>
2020-02-24 23:10:45 +0200

    Change method devise in application controller

--- app/controllers/application_controller.rb
+++ app/controllers/application_controller.rb
@@ -6,8 +6,8 @@ class ApplicationController < ActionController::Base
   before_action :set_new_game
 
   def configure_devise_params
-    devise_parameter_sanitizer.permit(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
-    devise_parameter_sanitizer.permit(:account_update) { |u| u.permit(:name, :password, :password_confirmation, :current_password) }
+    devise_parameter_sanitizer.for(:sign_up) { |u| u.permit(:name, :email, :password, :password_confirmation) }
+    devise_parameter_sanitizer.for(:account_update) { |u| u.permit(:name, :password, :password_confirmation, :current_password) }
   end
 
   # Болванка новой игры для кнопки "Начать игру", доступной на любой странице сайта
