2405b3257ebe757adc7a47f181a6cbf9ba75005b
SergUdo <sergii.udovichenko@gmail.com>
2020-02-24 10:48:05 +0200

    Modified spec helper for use version Ruby-2.6.5

--- spec/support/my_spec_helper.rb
+++ spec/support/my_spec_helper.rb
@@ -1,5 +1,19 @@
 module MySpecHelper
 
+  if RUBY_VERSION>='2.6.0'
+  if Rails.version < '5'
+    class ActionController::TestResponse < ActionDispatch::TestResponse
+      def recycle!
+        # hack to avoid MonitorMixin double-initialize error:
+        @mon_mutex_owner_object_id = nil
+        @mon_mutex = nil
+        initialize
+      end
+    end
+  else
+    puts "Monkeypatch for ActionController::TestResponse no longer needed"
+  end
+
   # наш хелпер, для населения базы нужным количеством рандомных вопросов
   def generate_questions(number)
     number.times do
@@ -9,6 +23,7 @@ module MySpecHelper
 end
 
 
-RSpec.configure do |c|
-  c.include MySpecHelper
+  RSpec.configure do |c|
+    c.include MySpecHelper
+  end
 end
