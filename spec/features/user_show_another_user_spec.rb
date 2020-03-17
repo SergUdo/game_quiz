a9b9f0e04c8623b65c1d24c73b5d6cfb3ad7090b
SergUdo <sergii.udovichenko@gmail.com>
2020-03-17 18:39:14 +0200

    Add function click_on

--- spec/features/user_show_another_user_spec.rb
+++ spec/features/user_show_another_user_spec.rb
@@ -1,25 +1,26 @@
 
 require 'rails_helper'
 
-  RSpec.feature 'USER show another user game', type: :feature do
+RSpec.feature 'USER show another user game', type: :feature do
 
-    let(:user) { FactoryGirl.create :user }
+  let(:user) { FactoryGirl.create :user }
 
-    let!(:game_another_user) { FactoryGirl.create(:game) }
+  let!(:game_another_user) { FactoryGirl.create(:game) }
 
-    before(:each) do
-     login_as user
-    end
+  before(:each) do
+   login_as user
+  end
 
-  scenario 'successfully' do
-     visit user_path(1)
-     expect(page).to have_current_path user_path(1)
-     expect(page).to have_content('Жора')
-     expect(page).to have_content('1')
-     expect(page).to have_content('Дата')
-     expect(page).to have_content('Новая игра')
-     expect(page).to have_content('Выигрыш')
-     expect(page).to have_content('Подсказки')
-     expect(page).to have_content('Вопрос')
-   end
+scenario 'successfully' do
+   visit user_path(1)
+   click_on('Новая игра')
+   expect(page).to have_current_path user_path(1)
+   expect(page).to have_content('Жора')
+   expect(page).to have_content('1')
+   expect(page).to have_content('Дата')
+   expect(page).to have_content('Новая игра')
+   expect(page).to have_content('Выигрыш')
+   expect(page).to have_content('Подсказки')
+   expect(page).to have_content('Вопрос')
  end
+end
