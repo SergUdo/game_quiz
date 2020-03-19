38f8b3b13d249ecfc8298ff32dac15e3bb8939f4
SergUdo <sergii.udovichenko@gmail.com>
2020-03-19 11:20:29 +0200

    Update integration test

--- spec/features/user_show_another_user_spec.rb
+++ spec/features/user_show_another_user_spec.rb
@@ -2,7 +2,7 @@
 require 'rails_helper'
 
 RSpec.feature 'USER show another user game', type: :feature do
-  let(:user) { FactoryGirl.create :user }
+  let(:user) { FactoryGirl.create :user, name: 'David'}
   let!(:game_another_user) { FactoryGirl.create(:game) }
 
   before(:each) do
@@ -10,9 +10,9 @@ RSpec.feature 'USER show another user game', type: :feature do
   end
 
   scenario 'successfully' do
-    visit user_path(1)
-    click_on('Жора')
-    expect(page).to have_content('Жора')
+    visit '/'
+    click_on('David')
+    expect(page).to have_content('David')
     expect(page).to have_content('Выйти')
     expect(page).to have_content('Хороший программист')
     expect(page).to have_content('Новая игра')
